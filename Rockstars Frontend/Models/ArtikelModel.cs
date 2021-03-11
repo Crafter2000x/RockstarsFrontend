4810e1cc2b868846b058d37126aa62dd954cbf5d
Bart <vannimwegenbart@gmail.com>
2021-03-11 15:45:31 +0100

    ArtikelPagina verder gemaakt

---
+++ Rockstars Frontend/Models/ArtikelModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rockstars_Frontend.Models
+{
+    public class ArtikelModel
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+        public int status { get; set; }
+        
+        // Thumbnail
+
+        // PDF
+
+        public DateTime createDate { get; set; }
+
+    }
+}
