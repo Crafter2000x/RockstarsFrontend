d4e71fab59a21a272acde4fe481f957e8e7d40e7
Connor Timmerman <457736@student.fontys.nl>
2021-06-10 14:39:08 +0200

    updated OnDemand to work with infinity scroll

---
+++ Rockstars Frontend/wwwroot/js/InfinityScrollC.js
@@ -0,0 +1,46 @@
+﻿var Page = 0;
+var NoMoredata = false;
+var inProgress = false;
+
+$(document).ready(function () {
+
+    LoadPartialViewArtikel();
+
+    $(window).on("scroll", function () {
+        var docHeight = $(document).height();
+        var winScrolled = $(window).height() + $(window).scrollTop();
+
+        if ((docHeight - winScrolled) < 1 && NoMoredata == false && inProgress == false) {
+
+            console.log("module scrolled to bottom");
+
+            inProgress = true;
+            LoadPartialViewArtikel();
+        }
+    });
+})
+
+function LoadPartialViewArtikel()
+{
+    /* Request the partial view with .get request. */
+    $.get('/Home/RequestOnDemandPartial/?Page=' + Page, function (data) {
+
+        if (data.trim() == '') {
+
+            NoMoredata = true;
+
+        } else {
+
+            $("#loadingdiv").show();
+            $('#WaitingForContent').hide();
+            /* data is the pure html returned from action method, load it to your page */
+            $('#partialPlaceHolder').append(data);
+            /* little fade in effect */
+            $('#partialPlaceHolder').fadeIn('fast');
+            console.log(Page);
+            Page++;
+            inProgress = false;
+            $("#loadingdiv").hide();
+        }  
+    });
+}
